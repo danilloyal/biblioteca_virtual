b1ea39611e705914a7b5442375d87e3671d2527c
Danillo Leal <tmdanillomartins@gmail.com>
2023-11-18 18:45:40 -0300

    Estrutura básica landingpage.

---
+++ src/main/java/com/br/bibiotecavirtual/BibiotecavirtualApplication.java
@@ -0,0 +1,13 @@
+package com.br.bibiotecavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibiotecavirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibiotecavirtualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/bibiotecavirtual/BibiotecavirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.bibiotecavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibiotecavirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
